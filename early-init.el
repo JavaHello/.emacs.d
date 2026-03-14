42a317fd9e4338e95ffe3a7900cc5c9a8b098f2b
luokai <kaiv2@outlook.com>
2026-03-14 11:46:09 +0800

    feat: add startup optimizations and refine line number handling

--- early-init.el
+++ early-init.el
@@ -1,3 +1,6 @@
+(setq load-prefer-newer t)
+(setq inhibit-startup-screen t)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -9,21 +12,16 @@
 ;; (add-to-list 'default-frame-alist '(internal-border-width . 0))
 
 (setq display-line-numbers-type 'relative)
-(defvar display-line-numbers-exempt-modes nil)
-;; 这些模式不要显示行号
-(dolist (mode '(term-mode
-                vterm-mode
-                shell-mode
-                eshell-mode
-                special-mode
-                shell-command-mode))
-  (add-to-list 'display-line-numbers-exempt-modes mode))
-
 (global-display-line-numbers-mode t)
 
-(defun my/disable-line-numbers ()
-  (setq-local display-line-numbers nil))
 
-(add-hook 'vterm-mode-hook #'my/disable-line-numbers)
-(add-hook 'shell-command-mode-hook #'my/disable-line-numbers)
-(add-hook 'special-mode-hook #'my/disable-line-numbers)
+;; 额外保险：进入 vterm 时关闭
+(add-hook 'vterm-mode-hook
+          (lambda ()
+            (display-line-numbers-mode 0)))
+
+(defun my/disable-line-numbers-for-special-buffers ()
+  (when (member (buffer-name) '("*Shell Command Output*" "*Async Shell Command*"))
+    (display-line-numbers-mode 0)))
+
+(add-hook 'after-change-major-mode-hook #'my/disable-line-numbers-for-special-buffers)
